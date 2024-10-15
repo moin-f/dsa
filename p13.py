8b9e13e61c7ac8b2cd71c3dc8f43aab927c57107
moin-f <moinsaokh92@gmail.com>
2024-10-15 14:59:09 +0530

    Add files via upload

---
+++ p13.py
@@ -0,0 +1,14 @@
+# 1. Use python code to evaluate each of the following expression.
+# a. 20 modulus 2 +7 - (3+7)  * 20 /2
+a= 20%2+7-(3+7)*20/2
+print('a = ', a)
+
+# b. 30 x 10 floor division 3 +10 modulus 3
+b= 30*10//3+10%3
+print('b = ',b)
+
+# c. 2^5 -2^4 +4 floor division 4
+c = 2**5 - 2**4 + 4//4
+print ('c = ', c)
+
+
